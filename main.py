d1277d2163c329f4f405cd91ddbb69f79e717ce7
sofyiq <150921026+sofyiq@users.noreply.github.com>
2023-12-17 04:19:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -42,10 +42,12 @@ bot = Client(
 )
 
 app = Client(
-  name="savebot",
-  api_id=api_id, api_hash=api_hash,
-  bot_token=token, in_memory=True
+    "name",
+    api_id=26384753,
+    api_hash="d0df15edaf47d46b36747f8af2e11b6f",
+    bot_token="5249469006:AAEyC4hMWvcrZ8dTfY14dd_QTh7sqfroBO4"
 )
+
 #bot = app
 #app = bot
 
